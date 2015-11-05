c7706fb147b9f3dccb9c97269092d4f2a0d79a0a
伊藤直哉 <n_itou@itounaoya-no-MacBook-Air.local>
2015-11-05 17:21:40 +0900

    initial commit

---
+++ GameScene.swift
@@ -0,0 +1,19 @@
+import SpriteKit
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+       /* Called when a touch begins */
+        /*
+        for touch in touches {
+            let location = touch.locationInNode(self)
+        }
+        */
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+
+    }
+}
