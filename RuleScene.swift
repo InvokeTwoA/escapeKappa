b9360dfb1a68905ea2de4448b35d202afefeb74b
伊藤直哉 <n_itou@itounaoya-no-MacBook-Air.local>
2015-11-18 11:05:56 +0900

    initial commit

---
+++ RuleScene.swift
@@ -0,0 +1,9 @@
+//
+//  RuleScene.swift
+//  escapeKappa
+//
+//  Created by 伊藤直哉 on 2015/11/18.
+//  Copyright © 2015年 ria10. All rights reserved.
+//
+
+import Foundation
---
+++ SparkEmitterNode.swift
@@ -0,0 +1,9 @@
+//
+//  SparkEmitterNode.swift
+//  escapeKappa
+//
+//  Created by 伊藤直哉 on 2015/11/10.
+//  Copyright © 2015年 ria10. All rights reserved.
+//
+
+import Foundation
